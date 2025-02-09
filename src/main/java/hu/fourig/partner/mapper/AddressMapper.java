fa96d79fca68372875297e2ab53cad121dac16b5
Zoltán Kertész <zoltan.kertesz@capture.eu>
2025-02-09 11:41:17 +0100

    mapping fix

--- src/main/java/hu/fourig/partner/mapper/AddressMapper.java
+++ src/main/java/hu/fourig/partner/mapper/AddressMapper.java
@@ -3,10 +3,12 @@ package hu.fourig.partner.mapper;
 import hu.fourig.partner.dto.AddressDto;
 import hu.fourig.partner.entity.Address;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
 
     AddressDto map(Address address);
+    @Mapping(target = "partner", ignore = true)
     Address map(AddressDto addressDto);
 }
